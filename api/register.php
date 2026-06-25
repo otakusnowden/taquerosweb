0fcc464a0350b1a243ee06e8800e059e96a14fa1
jponce <joseponce001@gmail.com>
2026-06-25 10:12:32 -0600

    new project

--- api/register.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__ . '/../config/app.php';
-
-use App\Controllers\AuthController;
-
-(new AuthController())->register();
--- demo/wings/admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/auth.php';
-$_SESSION = [];
-session_destroy();
-header('Location: index.php');
-exit;
