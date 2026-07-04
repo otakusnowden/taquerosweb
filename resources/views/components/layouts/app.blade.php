a76aaafd774f5d3aacadc7c507bd1c473395b02a
jponce <joseponce001@gmail.com>
2026-07-03 20:51:19 -0600

    add google tag manager

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -54,6 +54,27 @@
     <noscript><img height="1" width="1" style="display:none"
     src="https://www.facebook.com/tr?id=2174449436682991&ev=PageView&noscript=1"
     /></noscript>
+
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-M54LSFV6');</script>
+    <!-- End Google Tag Manager -->
+
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-SKHTE94DQB"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-SKHTE94DQB');
+    </script>
+
 </head>
 <body class="min-h-screen bg-white text-slate-600 antialiased">
     {{-- Skip link for keyboard users --}}
